e054b2e313202a26772197c5bdc885cc033b8d06
Eronan <animeftw@live.co.uk>
2019-03-06 14:02:21 +1100

    Database Functionality
    Partly done, testing still to be done. Downloading the database is required.

--- app/src/main/java/com/example/ernesttran/typecoverage/MainActivity.java
+++ app/src/main/java/com/example/ernesttran/typecoverage/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.ernesttran.typecoverage;
 
+import android.database.DatabaseErrorHandler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.database.sqlite.*;
+import android.database.Cursor;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase allData;
+    String[] PokemonList;
+    String filesDir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //File Director
+        filesDir = getFilesDir().getPath();
+        Log.d("Output",filesDir + "/PkmnData.db");
+        /*
+        //Load Database
+        allData = SQLiteDatabase.openDatabase(filesDir + "/PKMNData.db",null,SQLiteDatabase.OPEN_READONLY, null);
+
+        //Load Pokemon Array
+        Cursor speciesList = allData.rawQuery("SELECT Species FROM  Pokemon", null);
+        PokemonList = new String[speciesList.getCount()];
+        speciesList.moveToFirst();
+        for (int i = 0; i < speciesList.getCount(); i++) {
+            PokemonList[i] = speciesList.getString(0);
+            speciesList.moveToNext();
+        }
+        if (!speciesList.isClosed()) speciesList.close();
+        */
     }
 }
