a9fb9770a4b13d50ec1dd24822b2f823ebec1460
Eronan <animeftw@live.co.uk>
2019-03-11 16:54:21 +1100

    Adding Ability TextViews

--- app/src/main/java/com/example/ernesttran/typecoverage/MainActivity.java
+++ app/src/main/java/com/example/ernesttran/typecoverage/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.database.sqlite.*;
 import android.database.Cursor;
+import android.transition.Visibility;
 import android.util.Log;
+import android.view.View;
 import android.widget.*;
 
 import java.io.IOException;
@@ -83,7 +85,10 @@ public class MainActivity extends AppCompatActivity {
     SQLiteDatabase allData;
     PKMNDatabaseHelper mDBHelper;
     HashMap<String, Integer> PokemonList = new HashMap<String, Integer>();
+    HashMap<String, Integer> MoveList = new HashMap<String, Integer>();
     String[] SpeciesNames;
+    String[] MoveNames;
+    ArrayAdapter<String> DefaultMoveAdapter;
     //</editor-fold>
 
     @Override
@@ -109,26 +114,43 @@ public class MainActivity extends AppCompatActivity {
         //</editor-fold>
 
         //<editor-fold desc="Get Names for Pokemon">
-        Cursor resultSet = allData.rawQuery("SELECT ID, Species FROM Pokemon",null);
-        int length = resultSet.getColumnCount();
+        Cursor speciesResults = allData.rawQuery("SELECT ID, Species FROM Pokemon",null);
+        int length = speciesResults.getCount();
+        SpeciesNames = new String[length];
 
         //Start on first Row
-        if (resultSet.moveToFirst()) {
+        if (speciesResults.moveToFirst()) {
             for (int i = 0; i < length; i++) {
                 //Get Information from Cursor
-                Integer currID = resultSet.getInt(0);
-                String currName = resultSet.getString(1);
+                Integer currID = speciesResults.getInt(0);
+                String currName = speciesResults.getString(1);
                 PokemonList.put(currName, currID);
-
+                SpeciesNames[i] = currName;
                 //Move to next Row
-                resultSet.moveToNext();
+                speciesResults.moveToNext();
             }
         }
 
         //Build Array Adapter
-        SpeciesNames = new String[PokemonList.size()];
-        PokemonList.keySet().toArray(SpeciesNames);
-        ArrayAdapter<String> nameAdapater = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, SpeciesNames);
+        ArrayAdapter<String> speciesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, SpeciesNames);
+        //</editor-fold>
+
+        //<editor-fold desc="Get Names for Moves">
+        Cursor moveResults = allData.rawQuery("SELECT ID, Move FROM Moves", null);
+        length = moveResults.getCount();
+        MoveNames = new String[length];
+
+        //Move to First Row
+        if (moveResults.moveToFirst()) {
+           //Load Data into HashMap
+            for (int i = 0; i < length; i++) {
+                Integer currID = moveResults.getInt(0);
+                String currName = moveResults.getString(1);
+                MoveList.put(currName, currID);
+                MoveNames[i] = currName;
+            }
+        }
+        DefaultMoveAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, MoveNames);
         //</editor-fold>
 
         //<editor-fold desc="Initialize Generation Widgets">
@@ -154,11 +176,21 @@ public class MainActivity extends AppCompatActivity {
         //<editor-fold desc="Initialize Pokemon 1 Widgets">
         ///Expand Moves
         pokemon1_ExpandButton = findViewById(R.id.button_ExpandMoves1);
+        pokemon1_ExpandButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                String species = pokemon1_EditText.getText().toString();
+                if (!PokemonList.containsKey(species) || pokemon1_MoveLayout.getVisibility() == View.VISIBLE) {
+                    pokemon1_MoveLayout.setVisibility(View.GONE);
+                } else {
+                    pokemon1_MoveLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
         ///Image
         pokemon1_Image = findViewById(R.id.imageView_party1);
         ///Species Name
         pokemon1_EditText = findViewById(R.id.textView_party1);
-        pokemon1_EditText.setAdapter(nameAdapater);
+        pokemon1_EditText.setAdapter(speciesAdapter);
         ///Move Group
         pokemon1_MoveLayout = findViewById(R.id.linearLayout_Moveset1);
         ///Move 1
@@ -174,11 +206,21 @@ public class MainActivity extends AppCompatActivity {
         //<editor-fold desc="Initialize Pokemon 2 Widgets">
         ///Expand Moves
         pokemon2_ExpandButton = findViewById(R.id.button_ExpandMoves2);
+        pokemon2_ExpandButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                String species = pokemon2_EditText.getText().toString();
+                if (!PokemonList.containsKey(species) || pokemon2_MoveLayout.getVisibility() == View.VISIBLE) {
+                    pokemon2_MoveLayout.setVisibility(View.GONE);
+                } else {
+                    pokemon2_MoveLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
         ///Image
         pokemon2_Image = findViewById(R.id.imageView_party2);
         ///Species Name
         pokemon2_EditText = findViewById(R.id.textView_party2);
-        pokemon2_EditText.setAdapter(nameAdapater);
+        pokemon2_EditText.setAdapter(speciesAdapter);
         ///Move Group
         pokemon2_MoveLayout = findViewById(R.id.linearLayout_Moveset2);
         ///Move 1
@@ -194,11 +236,21 @@ public class MainActivity extends AppCompatActivity {
         //<editor-fold desc="Initialize Pokemon 3 Widgets">
         ///Expand Moves
         pokemon3_ExpandButton = findViewById(R.id.button_ExpandMoves3);
+        pokemon3_ExpandButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                String species = pokemon3_EditText.getText().toString();
+                if (!PokemonList.containsKey(species) || pokemon3_MoveLayout.getVisibility() == View.VISIBLE) {
+                    pokemon3_MoveLayout.setVisibility(View.GONE);
+                } else {
+                    pokemon3_MoveLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
         ///Image
         pokemon3_Image = findViewById(R.id.imageView_party3);
         ///Species Name
         pokemon3_EditText = findViewById(R.id.textView_party3);
-        pokemon3_EditText.setAdapter(nameAdapater);
+        pokemon3_EditText.setAdapter(speciesAdapter);
         ///Move Group
         pokemon3_MoveLayout = findViewById(R.id.linearLayout_Moveset3);
         ///Move 1
@@ -214,11 +266,21 @@ public class MainActivity extends AppCompatActivity {
         //<editor-fold desc="Initialize Pokemon 4 Widgets">
         ///Expand Moves
         pokemon4_ExpandButton = findViewById(R.id.button_ExpandMoves4);
+        pokemon1_ExpandButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                String species = pokemon1_EditText.getText().toString();
+                if (!PokemonList.containsKey(species) || pokemon1_MoveLayout.getVisibility() == View.VISIBLE) {
+                    pokemon1_MoveLayout.setVisibility(View.GONE);
+                } else {
+                    pokemon1_MoveLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
         ///Image
         pokemon4_Image = findViewById(R.id.imageView_party4);
         ///Species Name
         pokemon4_EditText = findViewById(R.id.textView_party4);
-        pokemon4_EditText.setAdapter(nameAdapater);
+        pokemon4_EditText.setAdapter(speciesAdapter);
         ///Move Group
         pokemon4_MoveLayout = findViewById(R.id.linearLayout_Moveset4);
         ///Move 1
@@ -234,11 +296,21 @@ public class MainActivity extends AppCompatActivity {
         //<editor-fold desc="Initialize Pokemon 5 Widgets">
         ///Expand Moves
         pokemon5_ExpandButton = findViewById(R.id.button_ExpandMoves5);
+        pokemon5_ExpandButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String species = pokemon5_EditText.getText().toString();
+                if (!PokemonList.containsKey(species) || pokemon5_MoveLayout.getVisibility() == View.VISIBLE) {
+                    pokemon5_MoveLayout.setVisibility(View.GONE);
+                } else {
+                    pokemon5_MoveLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
         ///Image
         pokemon5_Image = findViewById(R.id.imageView_party5);
         ///Species Name
         pokemon5_EditText = findViewById(R.id.textView_party5);
-        pokemon5_EditText.setAdapter(nameAdapater);
+        pokemon5_EditText.setAdapter(speciesAdapter);
         ///Move Group
         pokemon5_MoveLayout = findViewById(R.id.linearLayout_Moveset5);
         ///Move 1
@@ -254,11 +326,21 @@ public class MainActivity extends AppCompatActivity {
         //<editor-fold desc="Initialize Pokemon 6 Widgets">
         ///Expand Moves
         pokemon6_ExpandButton = findViewById(R.id.button_ExpandMoves6);
+        pokemon6_ExpandButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                String species = pokemon6_EditText.getText().toString();
+                if (!PokemonList.containsKey(species) || pokemon6_MoveLayout.getVisibility() == View.VISIBLE) {
+                    pokemon6_MoveLayout.setVisibility(View.GONE);
+                } else {
+                    pokemon6_MoveLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
         ///Image
         pokemon6_Image = findViewById(R.id.imageView_party6);
         ///Species Name
         pokemon6_EditText = findViewById(R.id.textView_party6);
-        pokemon6_EditText.setAdapter(nameAdapater);
+        pokemon6_EditText.setAdapter(speciesAdapter);
         ///Move Group
         pokemon6_MoveLayout = findViewById(R.id.linearLayout_Moveset6);
         ///Move 1
@@ -272,4 +354,6 @@ public class MainActivity extends AppCompatActivity {
         //</editor-fold>
         //</editor-fold>
     }
+
+
 }
